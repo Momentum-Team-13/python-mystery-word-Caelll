92556110046a3eb02a1bff432c43ff448715c0a7
Cael Blackmon <cael.blackmon1@gmail.com>
2022-06-23 11:34:08 -0400

    Working game without the secret word displayed.

--- mystery_word.py
+++ mystery_word.py
@@ -4,12 +4,12 @@ def generate_word(text):
     random_word = random.choice(text)
     print('WELCOME TO MYSTERY WORD! The computer has selected a random word.\n')
     print('Can you guess a letter in the word? \n ')
-    print(random_word)
+    #print(random_word)
     return random_word
 # Displays the word to unveil
 def display_word(text):
     mystery_word = str('_'*len(text))
-    print(mystery_word)
+    #print(mystery_word)
     return mystery_word
 def user_guess(text):
     guess = input("guess a letter: ")
@@ -17,7 +17,7 @@ def user_guess(text):
         return guess
     else:
         print("That wasn't a single letter! Try again.")
-    user_guess()
+    return user_guess(text)
     
 def play_game():
     #Reads in the text as a list
@@ -33,7 +33,7 @@ def play_game():
     correct_guess = []
     incorrect_guess = []
     remaining_guesses = 8
-    print(word_to_guess)
+    #print(word_to_guess)
     word_to_guess = list(word_to_guess)
     print(word_to_guess)
     while remaining_guesses > 0 and "_" in word_to_guess:
@@ -44,18 +44,18 @@ def play_game():
                 if list(random_word)[letter_index] == guess_letter:
                     word_to_guess[letter_index] = guess_letter
 
-
             print(word_to_guess)
             print("Correct! Keep going")
         else:
             remaining_guesses -= 1
+            incorrect_guess.append(guess_letter)
             print(f"Incorrect. You have {remaining_guesses} guesses left.")
-            
-
+        all_guesses = set(correct_guess + incorrect_guess)
+        print(f"Letters already guessed: {' '.join(all_guesses)}")
     if "_" in word_to_guess:
-        print("Sorry, you didn't win this time. Try again.")
+        print(f"Sorry, you didn't win this time. The secret word was: {''.join(random_word)}. Try again.")
     else:
-        print(f" Congrats! The secret word was: {word_to_guess}. YOU WON!!")
+        print(f" Congrats! The secret word was: {''.join(word_to_guess)}. YOU WON!!")
 
 if __name__ == "__main__":
     play_game()
